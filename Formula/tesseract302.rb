4f51321ffc2eae45f6f52dbacc8ad22b48b6271a
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2016-02-05 17:31:59 +0900

    Import

---
+++ Formula/tesseract302.rb
@@ -0,0 +1,64 @@
+class Tesseract302 < Formula
+  desc "OCR (Optical Character Recognition) engine"
+  homepage "https://github.com/tesseract-ocr/"
+  url "https://github.com/tesseract-ocr/tesseract/archive/3.02.02.tar.gz"
+  sha256 "1566a78f81570e7dc656e881a0fa688fe1c700d4657041807f2160bf67fbb485"
+
+  conflicts_with "tesseract",
+                 :because => "Conflicts with tesseract in main repository."
+
+  option "with-all-languages", "Install recognition data for all languages"
+
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "libtiff" => :recommended
+  depends_on "leptonica"
+
+  fails_with :llvm do
+    build 2206
+    cause "Executable 'tesseract' segfaults on 10.6 when compiled with llvm-gcc"
+  end
+
+  resource "tessdata" do
+    url "https://github.com/tesseract-ocr/tessdata/archive/3.04.00.tar.gz"
+    sha256 "5dcb37198336b6953843b461ee535df1401b41008d550fc9e43d0edabca7adb1"
+  end
+
+  resource "eng" do
+    url "https://github.com/tesseract-ocr/tessdata/raw/3.04.00/eng.traineddata"
+    sha256 "c0515c9f1e0c79e1069fcc05c2b2f6a6841fb5e1082d695db160333c1154f06d"
+  end
+
+  resource "osd" do
+    url "https://github.com/tesseract-ocr/tessdata/raw/3.04.00/osd.traineddata"
+    sha256 "9cf5d576fcc47564f11265841e5ca839001e7e6f38ff7f7aacf46d15a96b00ff"
+  end
+
+  def install
+    # explicitly state leptonica header location, as the makefile defaults to /usr/local/include,
+    # which doesn't work for non-default homebrew location
+    ENV["LIBLEPT_HEADERSDIR"] = HOMEBREW_PREFIX/"include"
+
+    ENV.cxx11 if build.devel?
+
+    system "./autogen.sh"
+    system "./configure", "--disable-dependency-tracking", "--prefix=#{prefix}"
+    system "make", "install"
+    if build.with? "training-tools"
+      system "make", "training"
+      system "make", "training-install"
+    end
+    if build.with? "all-languages"
+      resource("tessdata").stage { mv Dir["*"], share/"tessdata" }
+    else
+      resource("eng").stage { mv "eng.traineddata", share/"tessdata" }
+      resource("osd").stage { mv "osd.traineddata", share/"tessdata" }
+    end
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/tesseract -v 2>&1")
+  end
+end
---
+++ README.md
@@ -0,0 +1,6 @@
+TesseractOCR 3.02.02 version Formula
+===
+
+This is an unofficial TesseractOCR 3.02.02 Formula.
+
+This Formula is marked `conflicts_with` homebrew main repository's `tesseract`. 
